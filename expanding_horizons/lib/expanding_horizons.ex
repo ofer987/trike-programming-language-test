5c3567854a6858e7ccc799dc5db2c0886ccb13d4
Dan Jakob Ofer <ofer987@gmail.com>
2016-12-08 23:07:26 -0500

    Implemented solution

--- expanding_horizons/lib/expanding_horizons.ex
+++ expanding_horizons/lib/expanding_horizons.ex
@@ -1,5 +1,16 @@
 defmodule ExpandingHorizons do
   def start(_type, _args) do
+    IO.gets("")
+    |> String.strip
+    |> to_charlist
+    |> reverse_print
+
     Supervisor.start_link [], strategy: :one_for_one
+  end
+
+  defp reverse_print([]), do: :ok
+  defp reverse_print([head|tail]) do
+    reverse_print tail
+    IO.write to_string [head]
   end
 end
